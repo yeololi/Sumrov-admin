1b082c6358c5f6045ec4cdccf43cb863cc405531
채운 <shrie0602@gmail.com>
2024-06-12 15:46:57 +0900

    ui 1

---
+++ lib/const/font.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+const TextStyle cg = TextStyle(
+  color: Colors.black,
+  fontFamily: 'CormorantGaramond',
+  fontSize: 20,
+);
+
+const TextStyle ag = TextStyle(
+  color: Colors.black,
+  fontFamily: 'AppleSDGothicNeo',
+  fontSize: 30,
+);
